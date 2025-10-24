7d12f455939638bdb0e56b99a1cb3193e0900e17
M1C3E4 <mackoo.oklinski@gmail.com>
2025-10-24 17:53:09 +0000

    Total commit

--- firstApp.py
+++ firstApp.py
@@ -2,5 +2,5 @@ liczba = 6
 zmiennoprzecinkowa = 0.9
 napis = "Tekst"
 logiczna = False
-
+print("Wesołych świąt")
 print(f"liczba:{liczba}, liczba zmiennoprzecinkowa: {zmiennoprzecinkowa}, napis: {napis}, logiczna: {logiczna}. Siemano kolano elo trzy dwa zero")
