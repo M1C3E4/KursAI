80b5ac3527c5de6ad55e3215ee6f5e99300babc5
M1C3E4 <mackoo.oklinski@gmail.com>
2025-10-24 15:13:52 +0000

    Pierwszy commit testowy z plikiem .py

---
+++ firstApp.py
@@ -0,0 +1,6 @@
+liczba = 6
+zmiennoprzecinkowa = 0.9
+napis = "Tekst"
+logiczna = False
+
+print(f"liczba:{liczba}, liczba zmiennoprzecinkowa: {zmiennoprzecinkowa}, napis: {napis}, logiczna: {logiczna}")
